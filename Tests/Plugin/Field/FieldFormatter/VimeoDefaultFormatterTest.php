737c67dfe87c612a1b9e6379486d3a2f782d17e5
Luis Cordova <cordoval@gmail.com>
2014-05-20 23:34:43 -0500

    fixing indentation

--- Tests/Plugin/Field/FieldFormatter/VimeoDefaultFormatterTest.php
+++ Tests/Plugin/Field/FieldFormatter/VimeoDefaultFormatterTest.php
@@ -4,52 +4,52 @@
  * @file
  * Content Test\Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter
  */
+
 class VimeoDefaultFormatterTest extends \PHPUnit_Framework_TestCase
 {
-    /** @var $formatter \Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter */
-    protected $formatter;
+  /** @var $formatter \Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter */
+  protected $formatter;
 
-    protected function setUp()
-    {
-        $this->formatter = $this->getMockBuilder('Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter')
-            ->disableOriginalConstructor()
-            ->getMock();
+  protected function setUp()
+  {
+    $this->formatter = $this->getMockBuilder('Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter')
+      ->disableOriginalConstructor()
+      ->getMock();
 
-        $this->formatter->expects($this->any())
-            ->method('t')
-            ->with($this->isType('string'))
-            ->will($this->returnCallback(function () {
-                $args = func_get_args();
-                return $args[0];
-            }));
+    $this->formatter->expects($this->any())
+      ->method('t')
+      ->with($this->isType('string'))
+      ->will($this->returnCallback(function () {
+        $args = func_get_args();
+          return $args[0];
+      }));
 
-        $this->formatter->expects($this->any())
-            ->method('getSetting')
-            ->will($this->returnValue('100'));
-    }
+    $this->formatter->expects($this->any())
+      ->method('getSetting')
+      ->will($this->returnValue('100'));
+  }
 
-    public function testToReturnIframeVideo()
-    {
-        $id = $this->formatter->vimeoUrlToId('http://vimeo.com/45639028');
+  public function testToReturnIframeVideo()
+  {
+    $id = $this->formatter->vimeoUrlToId('http://vimeo.com/45639028');
 
-        $this->assertEquals('45639028', $id);
-    }
+    $this->assertEquals('45639028', $id);
+  }
 
-    public function testToGetSettingsForm()
-    {
-        $form_state = [];
-        $form = [];
-        $form_settings = $this->formatter->settingsForm($form, $form_state);
+  public function testToGetSettingsForm()
+  {
+    $form_state = [];
+    $form = [];
+    $form_settings = $this->formatter->settingsForm($form, $form_state);
 
-        $this->assertArrayHasKey('vimeo_width', $form_settings);
-        $this->assertArrayHasKey('vimeo_height', $form_settings);
-    }
+    $this->assertArrayHasKey('vimeo_width', $form_settings);
+    $this->assertArrayHasKey('vimeo_height', $form_settings);
+  }
 
-    public function testGetSummary()
-    {
-        $summary = $this->formatter->settingsSummary();
-
-        $this->assertContains('Width: 100px', $summary[0]);
-        $this->assertContains('Height: 100px', $summary[1]);
-    }
+  public function testGetSummary()
+  {
+    $summary = $this->formatter->settingsSummary();
+    $this->assertContains('Width: 100px', $summary[0]);
+    $this->assertContains('Height: 100px', $summary[1]);
+  }
 }
