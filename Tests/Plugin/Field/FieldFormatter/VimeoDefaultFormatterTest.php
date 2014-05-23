77991ef438e876b1056031f330ec9e6ecb6bc30e
Luis Montealegre <luis.montealegre@mandragora-web-systems.com>
2014-05-23 15:17:41 +0000

    Rewrote the code to parse a Vimeo ID from a given URL and added the corresponding tests. Added more tests.

--- Tests/Plugin/Field/FieldFormatter/VimeoDefaultFormatterTest.php
+++ Tests/Plugin/Field/FieldFormatter/VimeoDefaultFormatterTest.php
@@ -1,56 +1,255 @@
 <?php
-
 /**
  * @file
  * Content Test\Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter
  */
 
-class VimeoDefaultFormatterTest extends \PHPUnit_Framework_TestCase
+use \Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter;
+use \PHPUnit_Framework_TestCase;
+
+class VimeoDefaultFormatterTest extends PHPUnit_Framework_TestCase
 {
   /** @var $formatter \Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter */
   protected $formatter;
 
-  protected function setUp()
+  /**
+   * @param  array                                                                 $settings
+   * @return \Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter
+   */
+  protected function getVimeoFormatter(array $settings = [])
   {
-    $this->formatter = $this->getMockBuilder('Drupal\vimeo_field\Plugin\Field\FieldFormatter\VimeoDefaultFormatter')
-      ->disableOriginalConstructor()
-      ->setMethods(['t', 'getSetting'])
-      ->getMock();
+    $pluginDefinition = $this->getMockBuilder('\Drupal\Core\Field\FieldDefinitionInterface')
+                             ->getMock();
 
-    $this->formatter->expects($this->any())
-      ->method('t')
-      ->with($this->isType('string'))
-      ->will($this->returnCallback(function () {
-        $args = func_get_args();
-          return $args[0];
-      }));
-
-    $this->formatter->expects($this->any())
-      ->method('getSetting')
-      ->will($this->returnValue('100'));
+    return new VimeoDefaultFormatter('vimeo', '', $pluginDefinition, $settings, 'vimeo', '');
   }
 
-  public function testToReturnIframeVideo()
+  public function testShouldReturnVimeoIdFromValidUrl()
   {
-    $id = $this->formatter->vimeoUrlToId('http://vimeo.com/45639028');
+    $formatter = $this->getVimeoFormatter();
 
-    $this->assertEquals('45639028', $id);
+    $this->assertEquals('45639028', $formatter->parseVimeoId('http://vimeo.com/45639028'));
   }
 
-  public function testToGetSettingsForm()
+  /**
+   * @dataProvider invalidUrlProvider
+   */
+  public function testShouldThrowExceptionIfInvalidUrlIsProvided($url)
   {
-    $form_state = [];
+    $this->setExpectedException('\InvalidArgumentException');
+
+    $formatter = $this->getVimeoFormatter();
+
+    $formatter->parseVimeoId($url);
+  }
+
+  public function invalidUrlProvider()
+  {
+    return [
+      'email' => ['me@gmail.com'],
+      'file_path' => ['my_video.jpg'],
+    ];
+  }
+
+  /**
+   * @dataProvider invalidVimeoUrlProvider
+   */
+  public function testShouldThrowExceptionWithValidUrlThatIsNotAVimeoUrl($url)
+  {
+    $this->setExpectedException('\InvalidArgumentException');
+
+    $formatter = $this->getVimeoFormatter();
+
+    $formatter->parseVimeoId($url);
+  }
+
+  public function invalidVimeoUrlProvider()
+  {
+    return [
+      'ftp_url' => ['ftp://vimeo.com/7890139'],
+      'youtube_url' => ['http://youtube.com/3eRksdoias'],
+    ];
+  }
+
+  /**
+   * @dataProvider invalidVimeoUrlProvider
+   */
+  public function testShouldThrowExceptionWithValidVimeoUrlAndInvalidId($url)
+  {
+    $this->setExpectedException('\InvalidArgumentException');
+
+    $formatter = $this->getVimeoFormatter();
+
+    $formatter->parseVimeoId('http://vimeo.com/3eRksdoias');
+  }
+
+  public function testShouldReturnSettingsFormWithDefaultSettings()
+  {
     $form = [];
-    $form_settings = $this->formatter->settingsForm($form, $form_state);
+    $formState = [];
+    $translation = $this->getFormTranslation();
+
+    $formatter = $this->getVimeoFormatter();
+    $formatter->setStringTranslation($translation);
+
+    $form_settings = $formatter->settingsForm($form, $formState);
 
     $this->assertArrayHasKey('vimeo_width', $form_settings);
+    $this->assertEquals(600, $form_settings['vimeo_width']['#default_value']);
     $this->assertArrayHasKey('vimeo_height', $form_settings);
+    $this->assertEquals(400, $form_settings['vimeo_height']['#default_value']);
   }
 
-  public function testGetSummary()
+  public function testShouldReturnSettingsFormWithCustomSettings()
   {
-    $summary = $this->formatter->settingsSummary();
-    $this->assertContains('Width: 100px', $summary[0]);
-    $this->assertContains('Height: 100px', $summary[1]);
+    $form = [];
+    $formState = [];
+    $translation = $this->getFormTranslation();
+
+    $formatter = $this->getVimeoFormatter(['vimeo_width' => 100, 'vimeo_height' => 100]);
+    $formatter->setStringTranslation($translation);
+
+    $formSettings = $formatter->settingsForm($form, $formState);
+
+    $this->assertArrayHasKey('vimeo_width', $formSettings);
+    $this->assertEquals(100, $formSettings['vimeo_width']['#default_value']);
+    $this->assertArrayHasKey('vimeo_height', $formSettings);
+    $this->assertEquals(100, $formSettings['vimeo_height']['#default_value']);
+  }
+
+  /**
+   * @return \Drupal\Core\StringTranslation\TranslationInterface
+   */
+  protected function getFormTranslation()
+  {
+    $translation = $this->getMockBuilder('\Drupal\Core\StringTranslation\TranslationInterface')
+                          ->getMock();
+
+    $translation->expects($this->at(0))
+                ->method('translate')
+                ->with('Default width', [], [])
+                ->will($this->returnValue('Default width'));
+    $translation->expects($this->at(1))
+                ->method('translate')
+                ->with('None', [], [])
+                ->will($this->returnValue('None'));
+    $translation->expects($this->at(2))
+                ->method('translate')
+                ->with('Default height', [], [])
+                ->will($this->returnValue('Default height'));
+    $translation->expects($this->at(3))
+                ->method('translate')
+                ->with('None', [], [])
+                ->will($this->returnValue('None'));
+
+    return $translation;
+  }
+
+  public function testShouldGetDefaultSettings()
+  {
+    $this->assertEquals(['vimeo_width'  => 600, 'vimeo_height' => 400], VimeoDefaultFormatter::defaultSettings());
+  }
+
+  public function testShouldreturnSummaryWithDefaultSettings()
+  {
+    $formatter = $this->getVimeoFormatter();
+    $translation = $this->getSummaryTranslation();
+    $formatter->setStringTranslation($translation);
+
+    $summary = $formatter->settingsSummary();
+
+    $this->assertEquals('Width: 600px', $summary[0]);
+    $this->assertEquals('Height: 400px', $summary[1]);
+  }
+
+  public function testShouldreturnSummaryWithCustomSettings()
+  {
+    $formatter = $this->getVimeoFormatter(['vimeo_width' => 100, 'vimeo_height' => 100]);
+    $translation = $this->getSummaryTranslation();
+    $formatter->setStringTranslation($translation);
+
+    $summary = $formatter->settingsSummary();
+
+    $this->assertEquals('Width: 100px', $summary[0]);
+    $this->assertEquals('Height: 100px', $summary[1]);
+  }
+
+  /**
+   * @return \Drupal\Core\StringTranslation\TranslationInterface
+   */
+  protected function getSummaryTranslation()
+  {
+    $translation = $this->getMockBuilder('\Drupal\Core\StringTranslation\TranslationInterface')
+                        ->getMock();
+
+    $translation->expects($this->at(0))
+                ->method('translate')
+                ->with('Width: ', [], [])
+                ->will($this->returnValue('Width: '));
+
+    $translation->expects($this->at(1))
+                ->method('translate')
+                ->with('Height: ', [], [])
+                ->will($this->returnValue('Height: '));
+
+    return $translation;
+  }
+
+  public function testShouldReturnViewElementsWithDefaultSettings()
+  {
+    $formatter = $this->getVimeoFormatter();
+
+    $items = $this->getItems();
+
+    $viewElements = [
+      '#theme' => 'vimeo_player',
+      '#vids' => [
+        'vimeo' => [
+          'video' => '45639028',
+          'height' => 400,
+          'width' => 600,
+        ],
+      ],
+    ];
+
+    $this->assertEquals($viewElements, $formatter->viewElements($items));
+  }
+
+  public function testShouldReturnViewElementsWithCustomSettings()
+  {
+    $formatter = $this->getVimeoFormatter(['vimeo_width' => 100, 'vimeo_height' => 100]);
+
+    $items = $this->getItems();
+
+    $viewElements = [
+      '#theme' => 'vimeo_player',
+      '#vids' => [
+        'vimeo' => [
+          'video' => '45639028',
+          'height' => 100,
+          'width' => 100,
+        ],
+      ],
+    ];
+
+    $this->assertEquals($viewElements, $formatter->viewElements($items));
+  }
+
+  /**
+   * @return \Drupal\Core\TypedData\ListInterface
+   */
+  protected function getItems()
+  {
+    $vimeoItem = new ArrayObject(['processed' => 'http://vimeo.com/45639028']);
+    $vimeoItem->setFlags(ArrayObject::ARRAY_AS_PROPS);
+
+    $items = $this->getMockBuilder('\Drupal\Core\Field\FieldItemList')
+                  ->disableOriginalConstructor()
+                  ->getMock();
+    $items->expects($this->once())
+          ->method('getIterator')
+          ->will($this->returnValue(new ArrayObject(['vimeo' => $vimeoItem])));
+
+    return $items;
   }
 }
